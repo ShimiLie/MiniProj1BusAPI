e650890e0ac8973c46bf8c6e2529cfa82229d7ae
alie <anthonylie94@gmail.com>
2023-02-05 22:14:41 +0800

    fetched and rendered

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-function getApiData() {
-  
+function ApiData() {
+    const id = document.querySelector("#ID").value;
 
-    fetch(`https://arrivelah2.busrouter.sg/?id=83139`)
+  fetch(`https://arrivelah2.busrouter.sg/?id=${id}`)
       .then(response => response.json())
-      .then((json) => console.log(json)
+      .then((data) => console.log(data)
       )
       .catch((err)=> {
         console.log("ID  not found", err);
       })
 };
 
-getApiData();
+ApiData();
+
+
+function render() {
+    const id = document.querySelector("#ID").value;
+
+    fetch(`https://arrivelah2.busrouter.sg/?id=${id}`)
+    .then(response => response.json())
+    .then((data) => {
+
+       for (let i=0; i < data.services.length; i++) {
+        document.querySelector(".card-body").innerHTML= `
+        <h5 class="card-title">Bus No:${data.services[i].no}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Operator:${data.services[i].operator}</h6>
+        <p class="card-text">Next timing:${data.services[i].next.time}</p>
+        <p class="card-link">Subsequent timing:${data.services[i].next2.time}</p>
+        
+  `;
+       }
+           
+    })
+}
